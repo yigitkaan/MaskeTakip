e9c2c51146aafc997b1e41b7edaee098df58bb12
yigitkaan <kaantopcu756@gmail.com>
2024-02-21 12:28:30 +0300

    Ders Çalışması bitti

---
+++ Workaround/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.FirstName = "YİĞİT";
+        person.LastName = "TOPCU";
+        person.BirthOfDateYear = 1994;
+        person.NationalIdentity = 123;
+
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person);
+    }
+}
